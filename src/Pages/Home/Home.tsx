f6f8cb727a74e90f32e9d67288db57e5fe811bfc
Vinay Sharma <vinaysharma@Vinays-MacBook-Pro.local>
2022-09-18 18:11:48 +0530

    working on footer

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -32,6 +32,20 @@ const Home = (props: Props) => {
                         <button className='btn btn-outline-primary'>Learn about me</button>
                     </div>
                 </div>
+                <div className="row d-flex justify-content-center align-item-center p-5 location">
+                    <div className="col-md-12">
+                        <h2 className="text-center">My <span className='live'>Live</span> Location</h2>
+                    </div>
+                    <div className="col-md-5 p-2">
+
+                    </div>
+                    <div className="col-md-5 right p-4">
+                        <h2>Head Office:</h2>
+                        <p>25 first street, 2nd floor</p>
+                        <p>New Delhi, India</p>
+                        <p>201001</p>
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
